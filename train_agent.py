a1def2a421c961a75c0335ce985cc4a3f39aa48b
caicai0402 <caicaihu0402@gmail.com>
2025-03-16 18:27:25 +0800

    clean train_agent.py

--- train_agent.py
+++ train_agent.py
@@ -3,6 +3,15 @@ import numpy as np
 from simple_custom_taxi_env import SimpleTaxiEnv
 from utils import load_q_table, store_q_table
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--grid_size", type=int, default=5, help="Size of the grid environment")
+    parser.add_argument("--fuel_limit", type=int, default=5000, help="Maximum fuel available for the agent")
+    parser.add_argument("--obstacles_percentage", type=float, default=0.1, help="Percentage of grid occupied by obstacles (0.0 to 1.0)")
+    parser.add_argument("--pretrained_model", type=str, default=None, help="Path to the pretrained model for continued training")
+    parser.add_argument("--save_path", type=str, default="q_table.pkl", help="Path to save the training results")
+    return parser.parse_args()
+
 def get_state(obs):
     obstacle_north, obstacle_south, obstacle_east, obstacle_west = obs[10], obs[11], obs[12], obs[13]
     return (obstacle_south, obstacle_north, obstacle_east, obstacle_west)
@@ -10,12 +19,13 @@ def get_state(obs):
 def train_agent(env_config, pretrained_model=None, num_episodes=2000, alpha=0.99, gamma=0.01, 
                 epsilon_start=1.0, epsilon_end=0.1, epsilon_decay=0.9995):
     
+    env = SimpleTaxiEnv(**env_config)
     q_table = {} if pretrained_model is None else load_q_table(pretrained_model)
+
     epsilon = epsilon_start
     rewards_per_episode = []
-
+    
     for episode in range(num_episodes):
-        env = SimpleTaxiEnv(**env_config)
         obs, _ = env.reset()
         state = get_state(obs)
         
@@ -50,15 +60,6 @@ def train_agent(env_config, pretrained_model=None, num_episodes=2000, alpha=0.99
             print(f"🚀 Episode {episode + 1}/{num_episodes}, Average Reward: {avg_reward:.2f}, Epsilon: {epsilon:.4f}")
 
     return q_table
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--grid_size", type=int, default=5, help="Size of the grid environment")
-    parser.add_argument("--fuel_limit", type=int, default=5000, help="Maximum fuel available for the agent")
-    parser.add_argument("--obstacles_percentage", type=float, default=0.1, help="Percentage of grid occupied by obstacles (0.0 to 1.0)")
-    parser.add_argument("--pretrained_model", type=str, default=None, help="Path to the pretrained model for continued training")
-    parser.add_argument("--save_path", type=str, default="q_table.pkl", help="Path to save the training results")
-    return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
